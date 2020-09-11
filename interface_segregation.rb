2f19f038e0597a26cb43f29c2332d88f5a950522
Abraham Kuri <kurenn@icalialabs.com>
2020-09-11 16:10:00 -0500

    Adds class to work as interfaces for the motorcycle

--- interface_segregation.rb
+++ interface_segregation.rb
@@ -1,10 +1,21 @@
 # No client should be forced to depend on methods it does not use.
 
 class Motorcycle
+end
+
+class MotorcycleInternals
+  def initialize(motorcycle)
+  end
+
   def change_oil
   end
 
   def charge_battery
+  end
+end
+
+class Rideable
+  def initialize(motorcycle)
   end
 
   def drive
