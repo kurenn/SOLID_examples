9047cb4530fdc2505ca159d90d57000ebda12037
Abraham Kuri <kurenn@icalialabs.com>
2020-09-18 12:20:30 -0500

    Adds more information for dependency inversion example

--- dependency_inversion.rb
+++ dependency_inversion.rb
@@ -1,7 +1,11 @@
 # High-level modules should not depend on low-level modules. Both should depend on abstractions (e.g. interfaces).
 
 class Server
+  attr_accessor :provider
+
   def create
     Heroku.create
   end
 end
+
+# What if we needed to add another server, such as Digitalocean, GCP or AWS
