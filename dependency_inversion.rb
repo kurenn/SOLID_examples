f69f41f4317e21017734168592f1434fe6e2e4f9
Abraham Kuri <kurenn@icalialabs.com>
2020-09-18 12:32:43 -0500

    highlights the issue to solve

--- dependency_inversion.rb
+++ dependency_inversion.rb
@@ -3,15 +3,41 @@
 class Server
   attr_accessor :provider
 
+  def initialize(provider:)
+    @provider = provider
+  end
+
   def create
-    case provider do
-    when "heroku"
+    ServerCreator.new(self).perform
+  end
+end
+
+# What if we needed to add another server, such as Digitalocean, GCP or AWS
+
+module Hosting
+  class Heroku
+    def create
       Heroku.create
-    when "aws"
+    end
+  end
+
+  class AWS
+    def create
       node = AWS.create
       node.setup_ssh_access
     end
   end
 end
 
-# What if we needed to add another server, such as Digitalocean, GCP or AWS
+class ServerCreator
+  def initialize(server)
+    @server = server
+  end
+
+  def perform
+    provider_klass = "Hosting::#{server.provider.classify}".constantize
+    provider_klass.new.create
+  end
+end
+
+ServerCreator.new(Server.new(provider: "heroku")).perform
